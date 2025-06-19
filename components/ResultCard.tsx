2e6299b3f46eb7a9eb3dbef8215fb34c277bdf2c
salmankavanur <mpsalman2011@gmail.com>
2025-06-19 18:27:34 +0530

    JSX Syntax error fixed

--- components/ResultCard.tsx
+++ components/ResultCard.tsx
@@ -1,4 +1,6 @@
-"use client"
+"use client";
+
+import React from 'react';
 
 interface ScoringComponent {
   key: string
@@ -182,7 +184,7 @@ export default function ResultCard({
                             </div>
                           );
                         })
-                      }
+                      )}
                     </div>
                   </div>
 
